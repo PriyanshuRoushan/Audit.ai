efba1172c3adc8247848adf6b285a4c266779888
PriyanshuRoushan <priyanshuroushan01@gmail.com>
2026-05-07 22:39:01 +0530

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Audit AI 🚀</h1>
+      <p>Your AI Powered Audit Platform</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
