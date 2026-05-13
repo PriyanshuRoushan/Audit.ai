ee4eca5e4a6012f475004642c4004140767d34e4
PriyanshuRoushan <priyanshuroushan01@gmail.com>
2026-05-13 21:25:03 +0530

    changed for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const API = import.meta.env.VITE_API_URL;
+
+fetch(`${API}/api/auth/register`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data)
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
